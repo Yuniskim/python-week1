18c1ad6033e16ed17caaea013a6c6cc2ffdb1382
yuniskim <yuniskimani2015@gmail.com>
2024-07-18 03:08:06 -0700

    week1

---
+++ input.py
@@ -0,0 +1,9 @@
+ #Prompting user for name, age, and location
+name = input("hello name ")
+age = input("you are years old ")
+location = input("and live in ")
+
+# Printing personalized message
+print ('hello name')
+print ('you are years old')
+print ('and live in')
